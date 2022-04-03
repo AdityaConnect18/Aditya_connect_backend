333c29b4976796f5f98480de5f19074e3419e8e0
charankurru <61787496+charankurru@users.noreply.github.com>
2022-04-03 21:29:21 +0530

    done

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (!err) {
-        console.log('MongoDB connection succeeded.');
+        console.log('MongoDB connection succeeded.' + process.env.MONGODB_URI);
     } else {
         console.log(
             'Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)
